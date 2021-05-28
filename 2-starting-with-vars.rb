38f19da90bfc7fceb8d3ad3069244f19e8559e2c
LeDragoX <plinio2xd@gmail.com>
2021-05-28 14:22:13 -0300

    Update

--- 2-starting-with-vars.rb
+++ 2-starting-with-vars.rb
@@ -1,9 +1,12 @@
+# ruby 2-starting-with-vars.rb -v
+# String Variable 'character_name' and 'character_age'
 character_name = "Plínio"
 character_age  = "70"
 
 puts ("There once was a man named " + character_name)
 puts ("he was " + character_age + " years old.")
 
+# Changing the variable in the running
 character_name = "John"
 
 puts ("He really liked the name " + character_name)
