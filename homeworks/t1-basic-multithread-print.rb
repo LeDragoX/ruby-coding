c8a16d89f292fdb21f8bfa343bf9e7e4e599dfb5
LeDragoX <plinio2xd@gmail.com>
2021-05-29 14:30:30 -0300

    start my version of the homework

---
+++ homeworks/t1-basic-multithread-print.rb
@@ -0,0 +1,9 @@
+# WIP
+def ThreadedPrinting(message)
+    puts (message)
+end
+
+lst = ['Programação Paralela', 'Instituto Federal Fluminense']
+for value in lst do
+    ThreadedPrinting(value)
+end
