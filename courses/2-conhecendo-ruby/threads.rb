7de0d26c05f01a3f1262f850c22db3ca7008ae1a
Plínio Larrubia <plinio2xd@gmail.com>
2021-06-10 15:49:07 -0300

    update visualization of threads

--- courses/2-conhecendo-ruby/threads.rb
+++ courses/2-conhecendo-ruby/threads.rb
@@ -1,33 +1,33 @@
 def thr1()
     thread = Thread.new do
-        puts "Thread #{self.object_id} iniciada!"
+        puts "|thr1| Thread #{self.object_id} iniciada!"
         3.times do |valor|
-            puts "#{valor+1}"
+            puts "|thr1| #{valor+1}"
             sleep 0.1
         end
     end
     
-    puts "Já criei a thread"
+    puts "|thr1| Já criei a thread"
     thread.join
 end
 
 def thr2()
     thread = Thread.new do
-        puts "Thread #{self.object_id} iniciada!"
+        puts "|thr2| Thread #{self.object_id} iniciada!"
         5.times do |valor|
-            puts valor
+            puts "|thr2| #{valor}"
             sleep 1
         end
     end
     
-    puts "já criei a thread"
+    puts "|thr2| já criei a thread"
     thread.join(1)  # Timeout
 end
 
 def thr3()
     proc = Proc.new do |parametro|
         parametro.times do |valor|
-            print "[#{valor + 1}/#{parametro}]"
+            print "|thr3| [#{valor + 1}/#{parametro}]\n"
             sleep 0.5
         end
     end
@@ -38,9 +38,9 @@ def thr3()
     end
     
     thread.join
-    puts "Terminado!"
+    puts "|thr3| Terminado!"
 end
 
-#thr1()
-#thr2()
+thr1()
+thr2()
 thr3()
