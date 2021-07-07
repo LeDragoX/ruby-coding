e8205777caaa226bf7b75ac9754b735f24d38f9f
Plínio Larrubia <plinio2xd@gmail.com>
2021-07-07 10:58:45 -0300

    Update threads.rb

--- courses/2-conhecendo-ruby/threads.rb
+++ courses/2-conhecendo-ruby/threads.rb
@@ -1,44 +1,45 @@
 def thr1()
     thread = Thread.new do
-        puts "|thr1| Thread #{self.object_id} iniciada!"
-        3.times do |valor|
-            puts "|thr1| #{valor+1}"
+        puts "|thr1| Thread[#{thread.object_id}]: Initialized!"
+        3.times do |value|
+            puts "|thr1| Thread[#{thread.object_id}]: #{value+1}"
             sleep 0.1
         end
     end
     
-    puts "|thr1| Já criei a thread"
+    puts "|thr1| Thread[#{thread.object_id}]: already created!"
     thread.join
 end
 
 def thr2()
     thread = Thread.new do
-        puts "|thr2| Thread #{self.object_id} iniciada!"
-        5.times do |valor|
-            puts "|thr2| #{valor}"
+        puts "|thr2| Thread[#{thread.object_id}]: Initialized!"
+        5.times do |value|
+            puts "|thr2| Thread[#{thread.object_id}]: #{value}"
             sleep 1
         end
     end
     
-    puts "|thr2| já criei a thread"
+    puts "|thr2| Thread[#{thread.object_id}]: already created!"
     thread.join(1)  # Timeout
 end
 
 def thr3()
-    proc = Proc.new do |parametro|
-        parametro.times do |valor|
-            print "|thr3| [#{valor + 1}/#{parametro}]\n"
+    proc = Proc.new do |parameter|
+        parameter.times do |value|
+            print "|thr3| [#{value + 1}/#{parameter}]\n"
             sleep 0.5
         end
     end
     
     thread = nil
-    5.times do |valor|
-        thread = Thread.new(valor, &proc)
+    5.times do |value|
+        thread = Thread.new(value, &proc)
+        puts "|thr3| New Thread: [#{thread.object_id}]"
     end
     
     thread.join
-    puts "|thr3| Terminado!"
+    puts "|thr3| Thread[#{thread.object_id}]: Terminated!"
 end
 
 thr1()
