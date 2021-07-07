b3a20eff374f0c0dccf1799413c71e7b4aba86bf
Plínio Larrubia <plinio2xd@gmail.com>
2021-07-07 12:31:04 -0300

    Add t3 (INCOMPLETE)

---
+++ homeworks/t3-threads-calculator2.rb
@@ -0,0 +1,65 @@
+def inputHandle(msg = "Input:", isInt = false)
+  print "#{msg + " "}"
+  if (isInt == true)
+    return gets.chomp.to_i
+  end
+  if (isInt == false)
+    return gets.chomp.to_f
+  end
+end
+
+def createThread(n, a, b, operation, name)
+  semaphore = Mutex.new
+  threads = []
+
+  threads << Thread.new do
+    counter      = 1
+    sleepLimit   = 10
+    sleepSeconds = rand(1..sleepLimit)
+
+    puts "Tempo que a thread[#{self.object_id}] #{name} irá dormir: #{sleepSeconds}"
+    semaphore.synchronize {
+      while (counter <= n)
+
+        sleep(sleepSeconds)
+        puts "[#{counter}x] #{name}: #{a.send(operation, b)}"
+        counter += 1
+
+        #semaphore.lock()
+        #semaphore.unlock()
+      end
+    }
+  end
+
+  threads.each {|t| t.join }
+end
+
+def __main__
+  n = 0
+  a = 0
+  b = 0
+
+  while !(n > 0)
+    n = inputHandle("Deseja executar quantas vezes (N > 0)?", true)
+  end
+
+  a = inputHandle("Digite o valor de A:")
+  while (b == 0)
+    b = inputHandle("Digite o valor de B (B != 0):")
+  end
+
+  puts "\nExecuções:  #{n}x,\nValor de A: #{a},\nValor de B: #{b}"
+
+  ops = {
+    :+ => "SOMA",
+    :- => "SUBTRACAO",
+    :* => "MULTIPLICACAO",
+    :/ => "DIVISAO"
+  }
+
+  ops.map { |op, name| createThread(n, a, b, op, name) }
+  .each(&:join)
+
+end
+
+__main__()
