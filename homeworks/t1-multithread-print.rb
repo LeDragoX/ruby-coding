76aafb69783968eac97428280759f29071d3fefb
Plínio Larrubia <plinio2xd@gmail.com>
2021-07-07 10:59:08 -0300

    Rework

--- homeworks/t1-multithread-print.rb
+++
@@ -1,3 +0,0 @@
-['Programação Paralela', 'Instituto Federal Fluminense']
-  .map { |message| Thread.new { puts "Thread [#{self.object_id}]: #{message}" } }
-  .each(&:join)
--- homeworks/t1-threads-print.rb
+++ homeworks/t1-threads-print.rb
@@ -1,16 +1,17 @@
-# Maybe this is not "Multithreaded", but it's using Threads at least
+# Adapted from: https://gist.github.com/giancarlopro/4530682df8e678bf72d7c8a2c6d1cec7
 
 class MultiMan
     attr_accessor :message
-
+    
     def initialize(message)
         @message = message
-
+        
         ThreadedPrinting()
     end
-
+    
     def ThreadedPrinting()
         for str in @message do
+            # Maybe this is not "Multithreaded", but it's using Threads at least
             thread = Thread.new do
                 puts "Thread [#{thread.object_id}]: #{str}"
                 #sleep 0.5
