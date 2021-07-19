2da3b96f556eb937574539dea51acf337208b7a6
Plínio Larrubia <plinio2xd@gmail.com>
2021-07-18 23:49:54 -0300

    Extreme refactor

--- homeworks/ex7-amdahl-gustafson-laws.rb
+++ homeworks/ex7-amdahl-gustafson-laws.rb
@@ -1,99 +1,55 @@
-def calcAmdahl(p_number, s)
-  puts "Amdahl's Law"
+def calcAndSave(processor_number, sequential_portion, formula, file_name, columns)
+  # puts "#{processor_number}, #{sequential_portion}, #{"%.2f" % formula.call(processor_number[0], sequential_portion[0])}, #{file_name}, #{columns}"
+  puts "#{file_name.capitalize()}'s Law"
   puts "==========================================================="
-  puts "N_Processors | Seq05% | Seq10% | Seq50% | Seq70% | Seq90% |"
+  puts "#{columns[0]} | #{columns[1]} | #{columns[2]} | #{columns[3]} | #{columns[4]} | #{columns[5]} |"
   puts "==========================================================="
+  speedups = []
+  processed_speedups = []
 
   # Run from the Project's root directory
-  File.open("homeworks/data/amdahl.csv", "w+") do |file|
-    file.write("N_Processors, Seq05%, Seq10%, Seq50%, Seq70%, Seq90%\n")
-    p_number.map do |n|
-      formula = lambda { |n, s| (1 / ( s + ((1 - s) / n ))).round(2) }
+  File.open("homeworks/data/#{file_name.downcase()}.csv", "w+") do |file|
+    file.write("#{columns[0]}, #{columns[1]}, #{columns[2]}, #{columns[3]}, #{columns[4]}, #{columns[5]}\n")
+    processor_number.map() do |processor_number|
+      sequential_portion.map() do |sequential_portion|
+        speedups << formula.call(processor_number, sequential_portion)
+      end
 
-      speedup5 = formula.call(n, s[0])
-      speedup10 = formula.call(n, s[1])
-      speedup50 = formula.call(n, s[2])
-      speedup70 = formula.call(n, s[3])
-      speedup90 = formula.call(n, s[4])
-
-      print "     #{n}       |" if (n < 10)
-      print "     #{n}      |" if (n >= 10 && n < 100)
-      print "     #{n}     |" if (n >= 100)
-
-      print " #{speedup5}%  |" if (speedup5 < 10)
-      print " #{speedup5}% |" if (speedup5 >= 10 && speedup5 <= 100)
-
-      print " #{speedup10}%  |" if (speedup10 < 10)
-      print " #{speedup10}%  |" if (speedup10 >= 10 && speedup10 <= 100)
-
-      print " #{speedup50}%  |" if (speedup50 < 10)
-      print " #{speedup50}%  |" if (speedup50 >= 10 && speedup50 <= 100)
-
-      print " #{speedup70}%  |" if (speedup70 < 10)
-      print " #{speedup70}%  |" if (speedup70 >= 10 && speedup70 <= 100)
-
-      print " #{speedup90}%   |" if (speedup90 < 10)
-      print " #{speedup90}%  |" if (speedup90 >= 10 && speedup90 <= 100)
-
+      print "     #{processor_number}       |" if (processor_number < 10)
+      print "     #{processor_number}      |" if (processor_number >= 10 && processor_number < 100)
+      print "     #{processor_number}     |" if (processor_number >= 100)
+      speedups.map() do |speedup|
+        print " #{"%.2f" % speedup}%  |" if (speedup < 10)
+        print " #{"%.2f" % speedup}% |" if (speedup >= 10 && speedup < 100)
+        processed_speedups << "%.2f" % speedup
+      end
       puts
-      file.write("#{n}, #{speedup5}%, #{speedup10}%, #{speedup50}%, #{speedup70}%, #{speedup90}%\n")
+      file.write("#{processor_number}, ")
+      processed_speedups.each_index() do |i|
+        file.write("#{processed_speedups[i]}, ") if (processed_speedups[i] != processed_speedups[-1])
+        file.write("#{processed_speedups[i]}") if (processed_speedups[i] === processed_speedups[-1])
+      end
+      file.write("\n")
+      # Clear the lists
+      speedups.clear && processed_speedups.clear
     end
-    puts "==========================================================="
-  end
-end
-
-def calcGustafson(p_number, alpha)
-  puts "Gustafson's Law"
-  puts "==========================================================="
-  puts "N_Processors | Seq05% | Seq10% | Seq50% | Seq70% | Seq90% |"
-  puts "==========================================================="
-
-  # Run from the Project's root directory
-  File.open("homeworks/data/gustafson.csv", "w+") do |file|
-    file.write("N_Processors, Seq05%, Seq10%, Seq50%, Seq70%, Seq90%\n")
-    p_number.map do |n|
-      formula = lambda { |n, curAlpha| (n + (curAlpha * ( 1 - n ) )).round(2) }
-
-      speedup5 = formula.call(n, alpha[0])
-      speedup10 = formula.call(n, alpha[1])
-      speedup50 = formula.call(n, alpha[2])
-      speedup70 = formula.call(n, alpha[3])
-      speedup90 = formula.call(n, alpha[4])
-
-      print "     #{n}       |" if (n < 10)
-      print "     #{n}      |" if (n >= 10 && n < 100)
-      print "     #{n}     |" if (n >= 100)
-
-      print " #{speedup5}%  |" if (speedup5 < 10)
-      print " #{speedup5}% |" if (speedup5 >= 10 && speedup5 < 100)
-
-      print " #{speedup10}%   |" if (speedup10 < 10)
-      print " #{speedup10}%  |" if (speedup10 >= 10 && speedup10 < 100)
-
-      print " #{speedup50}%   |" if (speedup50 < 10)
-      print " #{speedup50}%  |" if (speedup50 >= 10 && speedup50 < 100)
-
-      print " #{speedup70}%   |" if (speedup70 < 10)
-      print " #{speedup70}%  |" if (speedup70 >= 10 && speedup70 < 100)
-
-      print " #{speedup90}%   |" if (speedup90 < 10)
-      print " #{speedup90}%  |" if (speedup90 >= 10 && speedup90 < 100)
-
-      puts
-      file.write("#{n}, #{speedup5}%, #{speedup10}%, #{speedup50}%, #{speedup70}%, #{speedup90}%\n")
-    end
-    puts "==========================================================="
   end
 end
 
 def main()
+  file_names = ["amdahl", "gustafson"]
+  columns = ["N_Processors", "Seq05%", "Seq10%", "Seq50%", "Seq70%", "Seq90%"]
   # Processors number
   processor_number   = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 30, 40, 50, 80, 100]
   # In % sequential portion
   sequential_portion = [0.05, 0.1, 0.5, 0.7, 0.9]
+  # Amdahl's formula
+  formula_amdahl = lambda { |n, s| ( 1 / (s + ((1 - s) / n )) ).round(2) }
+  # Gustafson's formula
+  formula_gustafson = lambda { |n, alpha| ( n + (alpha * (1 - n)) ).round(2) }
 
-  calcAmdahl(processor_number, sequential_portion)
-  calcGustafson(processor_number, sequential_portion)
+  calcAndSave(processor_number, sequential_portion, formula_amdahl, file_names[0], columns)
+  calcAndSave(processor_number, sequential_portion, formula_gustafson, file_names[1], columns)
 end
 
 main()
